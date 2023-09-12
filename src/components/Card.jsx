76801049c77a44732a498c5df1aab66ccf1c33e4
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-09-12 17:16:32 -0400

    Initial settings

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div>
+      Pageviews $ /month Monthly Billing Yearly Billing 25% discount Unlimited
+      websites 100% data ownership Email reports Start my trial
+    </div>
+  );
+};
+export default Card;
